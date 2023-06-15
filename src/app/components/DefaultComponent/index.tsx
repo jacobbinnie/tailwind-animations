44cf6f88a610240acebeaf2ea040d2a72f58ced4
Jacob <jacob.binnie@centrapass.com>
2023-06-15 18:41:41 +1200

    feat: add navbar feat: add sorting tabs

--- src/app/components/DefaultComponent/index.tsx
+++ src/app/components/DefaultComponent/index.tsx
@@ -14,9 +14,9 @@ export const DefaultComponent: React.FC<DefaultComponentProps> = () => {
         also be a sales tool.
       </p>
       <div className="flex gap-3 mt-10 justify-left">
-        <a className="inline-flex items-center justify-center text-sm font-semibold text-black duration-200 hover:text-blue-500 focus:outline-none focus-visible:outline-gray-600">
-          <div className="flex gap-2 bg-black px-4 py-2 rounded-3xl transition-all hover:animate-gradient-green">
-            <span className="text-white">Get Code</span>
+        <a className="inline-flex items-center justify-center text-sm duration-200 focus:outline-none focus-visible:outline-gray-600">
+          <div className="flex gap-2 bg-black px-4 py-2 rounded-3xl transition-all hover:px-6 hover:animate-green-swoosh cursor-pointer">
+            <span className="text-white font-semibold">Get Code</span>
             <ArrowRightIcon width={16} color="white" />
           </div>
         </a>
