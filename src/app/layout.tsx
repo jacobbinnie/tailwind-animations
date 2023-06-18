c75782f3f3af16787761e137769ed1aa8185a9df
Jacob <jacob.binnie@centrapass.com>
2023-06-18 16:02:17 +1200

    feat: add google analytics tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,12 @@
+import Script from "next/script";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Tailwind Animations | Beautiful Buttons, Gradients & More",
+  title:
+    "Tailwind Animations | Beautiful Buttons, Gradients & More Built With Tailwind CSS",
   description: "by @jacobbinnie",
 };
 
@@ -15,6 +17,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        ></Script>
+        <Script
+          id="gtag"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+          `,
+          }}
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
