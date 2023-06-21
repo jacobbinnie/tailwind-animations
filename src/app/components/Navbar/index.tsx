98b700152dd43f4250330e0e012c0c6ece8ec0e8
Jacob <jacob.binnie@centrapass.com>
2023-06-21 16:50:12 +1200

    feat: add signout functionality

--- src/app/components/Navbar/index.tsx
+++ src/app/components/Navbar/index.tsx
@@ -1,9 +1,12 @@
+import { useAuth } from "@/app/authprovider";
 import { useRoute } from "@/app/routeprovider";
 
 interface NavbarProps {}
 
 function Navbar({}: NavbarProps) {
   const { handleSetPage } = useRoute();
+  const { user, signOut } = useAuth();
+
   return (
     <div className="w-full mx-auto bg-white border-b sticky top-0 z-20">
       <div className="relative flex flex-col w-full p-5 mx-auto bg-white md:items-center md:justify-between md:flex-row md:px-6 lg:px-8">
@@ -40,26 +43,56 @@ function Navbar({}: NavbarProps) {
             </svg>
           </button>
         </div>
-        <nav className="flex-col items-center flex-grow hidden md:pb-0 md:flex md:justify-end md:flex-row">
-          {/* <a className="px-2 py-2 text-sm text-gray-500 lg:px-6 md:px-3">
-            Pricing
-          </a> */}
+        {user ? (
+          <nav className="flex-col items-center flex-grow hidden md:pb-0 md:flex md:justify-end md:flex-row">
+            <div className="inline-flex items-center gap-4 list-none lg:ml-auto">
+              <p className="text-sm text-gray-300">My Account</p>
 
-          <div className="inline-flex items-center gap-2 list-none lg:ml-auto">
-            <button
-              onClick={() => handleSetPage(0)}
-              className="block px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 focus:outline-none focus:shadow-outline"
-            >
-              Sign in
-            </button>
-            <button
-              onClick={() => handleSetPage(1)}
-              className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-black rounded-full group focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-gray-700 active:bg-gray-800 active:text-white focus-visible:outline-black"
-            >
-              Get Access
-            </button>
-          </div>
-        </nav>
+              <a
+                onClick={() => signOut()}
+                className="relative cursor-pointer inline-flex items-center justify-center p-4 px-2 py-1 overflow-hidden font-medium text-black transition duration-300 ease-out border-2 border-b-4 border-black rounded shadow-md group"
+              >
+                <span className="absolute inset-0 flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full bg-black group-hover:translate-x-0 ease">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M14 5l7 7m0 0l-7 7m7-7H3"
+                    ></path>
+                  </svg>
+                </span>
+                <span className="absolute flex items-center justify-center text-sm w-full h-full text-black transition-all duration-300 transform group-hover:translate-x-full ease">
+                  Sign out
+                </span>
+                <span className="relative invisible">Button Text</span>
+              </a>
+            </div>
+          </nav>
+        ) : (
+          <nav className="flex-col items-center flex-grow hidden md:pb-0 md:flex md:justify-end md:flex-row">
+            <div className="inline-flex items-center gap-2 list-none lg:ml-auto">
+              <button
+                onClick={() => handleSetPage(0)}
+                className="block px-4 py-2 mt-2 text-sm text-gray-500 md:mt-0 focus:outline-none focus:shadow-outline"
+              >
+                Sign in
+              </button>
+              <button
+                onClick={() => handleSetPage(1)}
+                className="inline-flex items-center justify-center px-4 py-2 text-sm font-semibold text-white bg-black rounded-full group focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 hover:bg-gray-700 active:bg-gray-800 active:text-white focus-visible:outline-black"
+              >
+                Get Access
+              </button>
+            </div>
+          </nav>
+        )}
       </div>
     </div>
   );
--- src/app/pages/auth/index.tsx
+++ src/app/pages/auth/index.tsx
@@ -30,14 +30,14 @@ export const Auth: React.FC<AuthProps> = ({ tab }) => {
 
   return (
     <>
-      <div className="flex h-screen flex-col items-center bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-200 to-gray-300">
+      <div className="flex h-screen flex-col items-center bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-gray-700 via-gray-900 to-black">
         <Navbar />
         <div className="flex h-full w-full items-center justify-center p-5">
-          <div className="w-full h-min max-w-xl px-5 sm:p-10 py-10 flex flex-col rounded-xl gap-4 overflow-hidden bg-white shadow-lg">
-            <p className="mt-8 text-4xl sm:text-5xl font-extrabold tracking-tight text-black">
+          <div className="w-full border-2 h-min max-w-xl px-5 sm:p-10 py-10 flex flex-col rounded-xl gap-4 overflow-hidden bg-transparent shadow-lg">
+            <p className="text-4xl sm:text-5xl font-extrabold tracking-tight text-white">
               {view === "signin" ? "Sign in" : "Sign up"}
             </p>
-            <p className="max-w-xl mt-4 font-medium text-sm sm:text-lg text-black">
+            <p className="max-w-xl mt-4 font-medium text-sm sm:text-lg text-gray-300">
               {view === "signin"
                 ? "Enter your email and we'll send you a magic sign in link"
                 : "Unlock a beautiful library of tailwind animations for your next project. Enter your email to get started"}
@@ -55,7 +55,7 @@ export const Auth: React.FC<AuthProps> = ({ tab }) => {
                   onChange={(e) => setEmail(e.target.value)}
                   aria-label="Email address"
                   autoComplete="email"
-                  className="block w-full p-4 font-semibold text-sm sm:text-lg text-black bg-transparent border border-gray-300 appearance-none focus:border-gray-300 rounded-xl focus:outline-none focus:ring-transparent placeholder:text-slate-400rounded-xl"
+                  className="block w-full p-4 font-semibold text-sm sm:text-lg text-gray-300 bg-transparent border border-gray-300 appearance-none focus:border-gray-300 rounded-xl focus:outline-none focus:ring-transparent placeholder:text-slate-400rounded-xl"
                   placeholder="Enter your email..."
                   required={true}
                   type="email"
@@ -63,14 +63,14 @@ export const Auth: React.FC<AuthProps> = ({ tab }) => {
 
                 <button
                   disabled={!isEmail()}
-                  className="relative inline-flex items-center justify-center h-14 p-4 w-full py-4 overflow-hidden font-medium text-white transition duration-300 ease-out animate-rainbow-river rounded-xl shadow-md group"
+                  className="relative inline-flex items-center justify-center h-14 p-4 w-full py-4 overflow-hidden font-semibold text-white transition duration-300 ease-out animate-rainbow-river rounded-xl shadow-md group"
                 >
                   <span
                     className={clsx(
                       isEmail()
                         ? "mr-0 translate-x-0 animate-green-swoosh"
                         : "mr-2",
-                      "absolute flex items-center justify-center w-full h-full text-white duration-300 -translate-x-full ease"
+                      "absolute flex items-center justify-center w-full h-full text-gray-300 duration-300 -translate-x-full ease"
                     )}
                   >
                     <svg
@@ -99,8 +99,8 @@ export const Auth: React.FC<AuthProps> = ({ tab }) => {
                 </button>
               </div>
             </form>
-            <div className="mt-5 w-full flex">
-              <p className="text-sm sm:text-lg font-medium text-black">
+            <div className="w-full flex">
+              <p className="text-sm font-medium text-gray-300">
                 {view === "signin"
                   ? "Don't have an account?"
                   : "Already have an account?"}
