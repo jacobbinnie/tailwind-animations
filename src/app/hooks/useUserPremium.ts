f8dcd3f4792ff60ad74628d030fed7e189849d21
Jacob <jacob.binnie@centrapass.com>
2023-06-26 16:14:34 +1200

    feat: add useUserPremium hook and loader

---
+++ src/app/hooks/useUserPremium.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { isUserPremium } from "../firebase/isUserPremium";
+
+export const useUserPremium = (userId: string | undefined) => {
+  const [isPremium, setIsPremium] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchUserPremium = async () => {
+      // Simulating an asynchronous API call
+
+      try {
+        if (userId) {
+          const premium = await isUserPremium(userId); // Replace with your actual function call
+          setIsPremium(premium);
+          setLoading(false);
+        }
+      } catch (error) {
+        console.error("Error fetching user premium status:", error);
+      }
+    };
+
+    fetchUserPremium();
+  }, [userId]);
+
+  return { isPremium, loading };
+};
