888ae8c4d2fef7650853ff8c35bd9f2e12a91c1f
Jacob <jacob.binnie@centrapass.com>
2023-07-03 14:01:15 +1200

    feat: update routeDictator background

--- src/app/routeprovider/RouteDictator.tsx
+++ src/app/routeprovider/RouteDictator.tsx
@@ -33,7 +33,7 @@ export function RouteDictator({}: RouteDictatorProps) {
     return (
       <>
         <Navbar />
-        <div className="min-h-screen w-full flex flex-col justify-center items-center bg-gradient-to-r from-[#5046e5] to-[#6a14d1]">
+        <div className="min-h-screen w-full flex flex-col justify-center items-center bg-gradient-to-r from-[#5046e5] to-[#2f0761]">
           <div className="w-20 animate-spin">
             <svg
               version="1.1"
