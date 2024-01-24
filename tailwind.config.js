b7320f8f05d282f4c98570f5c4988be6bb4dea14
Tyler Hilbert <TylerJoshuaHilbert@gmail.com>
2024-01-24 08:56:47 -0500

    wobble-triangles + blue-swoosh

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "hover:animate-green-swoosh 2s ease infinite",
     "hover:animate-serene-sunset 2s ease infinite",
     "hover:animate-black-swoosh 2s ease infinite",
+    "hover:animate-blue-swoosh 2s ease infinite",
     "hover:animate-purple-swoosh 2s ease infinite",
     "hover:animate-tranquil-waters 5s ease infinite",
     "hover:animate-mellow-embers 2s ease infinite",
@@ -68,6 +69,7 @@ module.exports = {
         "green-swoosh": "green-swoosh 2s ease infinite",
         "serene-sunset": "serene-sunset 2s ease infinite",
         "black-swoosh": "black-swoosh 2s ease infinite",
+        "blue-swoosh": "blue-swoosh 2s ease infinite",
         "purple-swoosh": "purple-swoosh 2s ease infinite",
         "tranquil-waters": "tranquil-waters 5s ease infinite",
         "mellow-embers": "mellow-embers 2s ease infinite",
@@ -88,6 +90,7 @@ module.exports = {
         "mystic-aura": "mystic-aura 2s ease infinite",
         "vibrant-sun": "vibrant-sun 2s ease infinite",
         "shifting-triangles": "shifting-triangles 2s ease infinite",
+        "wobble-shifting-triangles": "wobble-shifting-triangles 2s ease infinite",
 
         // MOVEMENTS
         "morphing-rectangle": "morphing-rectangle 2s ease infinite",
@@ -208,6 +211,27 @@ module.exports = {
             "background-position": "left center",
             "background-image":
               "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(128, 128, 128, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+        },
+
+        "blue-swoosh": {
+          "0%": {
+            "background-size": "600% 100%",
+            "background-position": "left center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+          "50%": {
+            "background-size": "600% 100%",
+            "background-position": "right center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8))",
+          },
+          "100%": {
+            "background-size": "600% 100%",
+            "background-position": "left center",
+            "background-image":
+              "linear-gradient(to right, rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8), rgba(64, 64, 64, 0.8), rgba(0, 0, 255, 0.8), rgba(0, 0, 0, 0.8))",
           },
         },
 
@@ -640,6 +664,32 @@ module.exports = {
               "linear-gradient(60deg, rgba(255, 0, 0, 0.8) 25%, rgba(0, 255, 0, 0.8) 50%, rgba(0, 0, 255, 0.8) 75%)",
             "background-size": "200% 200%",
             "background-position": "top left",
+          },
+        },
+
+        "wobble-shifting-triangles": {
+          "0%": {
+            "background-image":
+              "linear-gradient(60deg, rgba(255, 0, 0, 0.8) 25%, rgba(0, 255, 0, 0.8) 50%, rgba(0, 0, 255, 0.8) 75%)",
+            "background-size": "200% 200%",
+            "background-position": "top left",
+            "transform": "rotate(-5deg)",
+          },
+          "25%": { transform: "rotate(5deg)" },
+          "50%": {
+            "background-image":
+              "linear-gradient(60deg, rgba(0, 0, 255, 0.8) 25%, rgba(255, 0, 0, 0.8) 50%, rgba(0, 255, 0, 0.8) 75%)",
+            "background-size": "200% 200%",
+            "background-position": "bottom right",
+            "transform": "rotate(-5deg)"
+          },
+          "75%": { transform: "rotate(5deg)" },
+          "100%": {
+            "background-image":
+              "linear-gradient(60deg, rgba(255, 0, 0, 0.8) 25%, rgba(0, 255, 0, 0.8) 50%, rgba(0, 0, 255, 0.8) 75%)",
+            "background-size": "200% 200%",
+            "background-position": "top left",
+            "transform": "rotate(-5deg)",
           },
         },
 
